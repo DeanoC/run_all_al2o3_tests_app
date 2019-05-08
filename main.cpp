3e21d133a6ff50f0b76682f87299168eca5669ea
deano <me@deanoc.com>
2019-05-08 10:19:48 +0300

    Enumerate tests

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include "al2o3_platform/platform.h"
 #include "al2o3_os/filesystem.hpp"
 
+void RunTestFunc(Os_DirectoryEnumeratorHandle handle, void* userData, char const* filename) {
+	LOGINFOF("Running %s test", filename);
+	if (strncmp(filename, "test_", 5) == 0) {
+		Os_SystemRun(filename, 0, nullptr);
+	}
+}
 int main(int argv, char* argc[]) {
+	LOGWARNING("Parsing folder for test_*");
+	Os_DirectoryEnumeratorHandle handle = Os_DirectoryEnumeratorFromPath("./", &RunTestFunc, nullptr);
+	Os_DirectoryEnumeratorSyncStart(handle);
+	while (Os_DirectoryEnumeratorSyncNext(handle)) {
+		// do nothing
+	}
+	Os_DirectoryEnumeratorClose(handle);
 	return 0;
 }
