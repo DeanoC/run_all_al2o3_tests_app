5de992bfef8d197d4fedd9ee30477e9fc915088a
Dean Calver <me@deanoc.com>
2019-07-05 10:16:20 +0300

    Fix test runner

--- main.cpp
+++ main.cpp
@@ -2,25 +2,9 @@
 #include "al2o3_os/filesystem.hpp"
 #include "utils_simple_logmanager/logmanager.h"
 
-static int testsRan = 0;
 
 
 void RunTestFunc(Os_DirectoryEnumeratorHandle handle, void* userData, char const* filename) {
-	if (strncmp(filename, "test_", 5) == 0) {
-#if AL2O3_PLATFORM == AL2O3_PLATFORM_WINDOWS
-		size_t namePos;
-		size_t extPos;
-		Os_SplitPath(filename, &namePos, &extPos);
-		if (extPos == FS_npos) return;
-		if (strncmp(filename + extPos, "exe", 4) != 0) return;
-#endif
-		if (strncmp(filename, "test_data", 9) == 0) return;
-
-		LOGINFOF("Running %s test", filename);
-		Os_SystemRun(filename, 0, nullptr);
-
-		testsRan++;
-	}
 }
 int main(int argv, char* argc[]) {
 	// yes loggin and some filsystem (directory and system run) need to be
@@ -31,11 +15,30 @@ int main(int argv, char* argc[]) {
 
 	auto logger = SimpleLogManager_Alloc();
 
+	int testsRan = 0;
+
 	LOGINFO("Searching folder for test_*");
-	Os_DirectoryEnumeratorHandle handle = Os_DirectoryEnumeratorAlloc("./", &RunTestFunc, nullptr);
-	Os_DirectoryEnumeratorSyncStart(handle);
-	while (Os_DirectoryEnumeratorSyncNext(handle)) {}
-	Os_DirectoryEnumeratorFree(handle);
+
+	Os_DirectoryEnumeratorHandle handle = Os_DirectoryEnumeratorCreate("./");
+	while (Os_DirectoryEnumeratorItem const* entry = Os_DirectoryEnumeratorSyncNext(handle)) {
+		if (strncmp(entry->filename, "test_", 5) == 0) {
+#if AL2O3_PLATFORM == AL2O3_PLATFORM_WINDOWS
+			size_t namePos;
+			size_t extPos;
+			Os_SplitPath(filename, &namePos, &extPos);
+			if (extPos == FS_npos) return;
+			if (strncmp(filename + extPos, "exe", 4) != 0) return;
+#endif
+			if(entry->directory)
+				continue;
+
+			LOGINFOF("Running %s test", entry->filename);
+			Os_SystemRun(entry->filename, 0, nullptr);
+
+			testsRan++;
+		}
+	}
+	Os_DirectoryEnumeratorDestroy(handle);
 
 	LOGINFOF("%i test programs ran", testsRan);
 
